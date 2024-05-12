418bc62be4891e6583e7a71c7442dea011b64482
afoliveira5 <aparecidoferreiradeoliveira@gmail.com>
2024-05-12 19:29:35 -0300

    Teste subir no git action

---
+++ target/test-classes/br/ce/aparecido/RequisiçãoDelete.java
@@ -0,0 +1,49 @@
+package br.ce.aparecido;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import org.junit.Test;
+
+public class RequisiçãoDelete {
+	@Test
+	public void deveSalvarUsuárioAlef() {
+		given().log().all().contentType("application/json")
+				.body("{\"name\":\"Alef\",\"job\":\"ManageQualityAssurance\"}").when()
+				.post("https://reqres.in/api/users")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.body("id", is(notNullValue()))
+				.body("name", is("Alef"))
+				.body("job", is("ManageQualityAssurance"));
+
+	}
+	@Test
+	public void deletarUsuárioAlef() {
+		given().log().all().contentType("application/json")
+				.body("{\"name\":\"Alef\",\"job\":\"ManageQualityAssurance\"}").when()
+				.delete("https://reqres.in/api/users/2")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.body("id", is(notNullValue()))
+				.body("name", is("Alef"))
+				.body("job", is("ManageQualityAssuranceS"));
+
+	}
+	@Test
+	public void verificarUsuárioAlef() {
+		given().log().all().contentType("application/json")
+				.body("{\"name\":\"Alef\",\"job\":\"ManageQualityAssurance\"}").when()
+				.get("https://reqres.in/api/users/2")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.body("id", is(notNullValue()))
+				.body("name", is("Alef"))
+				.body("job", is("ManageQualityAssuranceS"));
+
+	}
+}
